d0a7f9130fbb0048b99dddb9e9da5b7699e8d4b6
HarshalGundaCuelogic <harshal.gunda@cuelogic.com>
2019-11-18 12:05:23 +0530

    minor changes like margin and padding in some sections

--- js/index.js
+++ js/index.js
@@ -40,22 +40,42 @@ $(document).ready(function(){
 });
 
 // Carousel code
-$('.carousel.carousel-multi-item .carousel-item').each(function(){
+// $('.carousel.carousel-multi-item .carousel-item').each(function(){
+//   var next = $(this).next();
+//   if (!next.length) {
+//     next = $(this).siblings(':first');
+//   }
+//   next.children(':first-child').clone().appendTo($(this));
+
+//   for (var i=0;i<4;i++) {
+//     next=next.next();
+//     if (!next.length) {
+//       next=$(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this));
+//   }
+// });
+
+// $('#carousel-example-multi').carousel({
+//   interval: 10000
+// })
+
+
+$('#theCarousel').carousel({
+  interval: 2000
+})
+
+$('.multi-item-carousel .item').each(function(){
   var next = $(this).next();
   if (!next.length) {
     next = $(this).siblings(':first');
   }
   next.children(':first-child').clone().appendTo($(this));
-
-  for (var i=0;i<4;i++) {
-    next=next.next();
-    if (!next.length) {
-      next=$(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
-
-$('#carousel-example-multi').carousel({
-  interval: 10000
-})
